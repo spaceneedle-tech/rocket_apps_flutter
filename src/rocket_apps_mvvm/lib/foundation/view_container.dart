c20042be34ec2c99405f197ed7b041c5cd757659
Luiz Felipe Alves Lima <felnanuke@gmail.com>
2024-07-03 17:15:34 -0300

    Update view_container.dart
    update to allow view from mvvm and view from other together

--- src/rocket_apps_mvvm/lib/foundation/view_container.dart
+++ src/rocket_apps_mvvm/lib/foundation/view_container.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/widgets.dart';
 import 'package:rocket_apps_mvvm/binding/bind.dart';
-import 'package:rocket_apps_mvvm/foundation/view.dart';
+import 'package:rocket_apps_mvvm/foundation/view.dart' as mv;
 import 'package:rocket_apps_mvvm/foundation/viewmodel.dart';
 
 // ignore: must_be_immutable
-class ViewContainer<TView extends View, TViewModel extends ViewModel>
+class ViewContainer<TView extends mv.View, TViewModel extends ViewModel>
     extends StatelessWidget {
   late TView view;
   late TViewModel viewModel;
